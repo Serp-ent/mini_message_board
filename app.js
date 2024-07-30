ebca5bbbcc9c57bb7ea8b30bb939c44cb72e58da
kenny <kenny909@mat.umk.pl>
2024-07-30 19:25:34 +0200

    Add simple error handling for message

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ app.use('/new', messageRouter);
 // TODO: this should have own route
 // additionaly there should be controllers that do the hard work
 app.get('/msgs/:id', (req, res) => {
+  const msg = messages.at(req.params.id);
+  if (!msg) {
+    throw new Error('No message with given ID');
+
+  }
   res.render('messageDetails', { message: messages.at(req.params.id) });
+});
+
+app.use((err, req, res, next) => {
+  res.send(err.message);
 });
 
 app.listen(PORT, () => {
